44c23a199884e421031b37dbe1f0d5a6996ff944
dumpsayamrat <sayamrat.kt@gmail.com>
2023-11-25 23:22:31 +0700

    fix: Fix build error

--- src/utils/photo.ts
+++ src/utils/photo.ts
@@ -5,7 +5,7 @@ export function bytesToSize(bytes: number): string {
 
   if (bytes === 0) return '0 Byte'
 
-  const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024)), 10))
+  const i = parseInt(String(Math.floor(Math.log(bytes) / Math.log(1024))), 10)
   return Math.round((bytes / Math.pow(1024, i)) * 100) / 100 + ' ' + sizes[i]
 }
 
