cf256ff735027aae0f10635f252a5ca351251bdb
dumpsayamrat <sayamrat.kt@gmail.com>
2024-06-27 22:23:15 +0700

    Support caching on cdn

--- next.config.js
+++ next.config.js
@@ -13,8 +13,44 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dumpsayamrat.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
     minimumCacheTTL: 31536000,
+  },
+  async headers() {
+    return [
+      {
+        source: '/admin/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
   },
 }
 
